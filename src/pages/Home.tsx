ea4180d1ac11eb1796ee45112e5c68ca38c47dfe
sidhantsuvagiya <66551578+sidhantsuvagiya@users.noreply.github.com>
2024-03-01 12:44:43 +0530

    upadted location page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,11 @@ import { CharacterType } from "../types/types";
 import Pagination from "../components/Pagination";
 import CharacterCard from "../components/CharacterCard"
 import Search from "../components/Search";
-import Filter from "../components/Filter";
-
 
 export const Home = () => {
 
     const [totalPages, setTotalPages] = useState(1);
     const [currentPage, setCurrentPage] = useState(1);
-    const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
     const [characterDetails, setCharacterDetails] = useState<CharacterType[]>([])
     const [searchQuery, setSearchQuery] = useState<string | null>(null);
     const [loading, setLoading] = useState(false);
@@ -22,30 +19,11 @@ export const Home = () => {
             setLoading(true);
             setError(null);
 
-            let apiUrl = "https://rickandmortyapi.com/api/character";
+            let apiUrl = `https://rickandmortyapi.com/api/character?page=${currentPage}`;
 
             if (searchQuery) {
-                apiUrl += `?name=${searchQuery}`;
+                apiUrl += `&name=${searchQuery}`;
             }
-
-            // Apply selectedFilter if available
-            if (selectedFilter) {
-                switch (selectedFilter) {
-                    case 'location':
-                        apiUrl = "https://rickandmortyapi.com/api/location";
-                        // Add logic for location filter
-                        break;
-                    case 'episode':
-                        apiUrl = "https://rickandmortyapi.com/api/episode";
-                        // Add logic for episodes filter
-                        break;
-                    // Add more cases for additional filters
-                    default:
-                        break;
-                }
-            }
-
-            apiUrl += `?page=${currentPage}`;
 
             const response = await fetch(apiUrl);
             const result = await response.json();
@@ -66,22 +44,15 @@ export const Home = () => {
         setSearchQuery(query);
     };
 
-    const handleSelectFilter = (filter: string) => {
-        setSelectedFilter(filter);
-        console.log(filter);
-
-    }
-
     useEffect(() => {
         getCharacterDetails(searchQuery || "")
-    }, [currentPage, searchQuery, selectedFilter])
+    }, [currentPage, searchQuery])
 
     if (loading) { return <div className="flex items-center justify-center p-8"><i className="fas fa-spinner fa-spin text-4xl"></i></div> }
 
     return (
         <div>
             <Search onSearch={handleSearch} />
-            <Filter selectedFilter={selectedFilter}  onSelectFilter={handleSelectFilter} />
             {error ? <div className="flex items-center justify-center p-8"><p className="text-red-500 font-medium">{error}</p></div> : (
                 <>
                     <div className="p-4 max-w-screen-xl mx-auto grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
--- src/pages/LocationPage.tsx
+++ src/pages/LocationPage.tsx
@@ -26,15 +26,27 @@ export const LocationPage = () => {
             setLoading(true);
 
             let apiUrl = "https://rickandmortyapi.com/api/location";
-            const response = await fetch(apiUrl);
-            const result = await response.json();
-            console.log("Locations:", result);
-            setLocations(result.results);
-            // Call getCharacterDetails for the first time
-            if (result.results.length > 0) {
-                await getCharacterDetails(result.results[0]);
-            }
+            let allLocations: LocationType[] = [];
 
+            do {
+                const response = await fetch(apiUrl);
+                const result = await response.json();
+                console.log("Locations:", result);
+
+                // Append the current page's locations to the array
+                allLocations = allLocations.concat(result.results);
+
+                // Update the apiUrl for the next page
+                apiUrl = result.info.next;
+
+            } while (apiUrl); // Continue fetching as long as there is a next page
+
+            setLocations(allLocations);
+
+            // Call getCharacterDetails for the first time
+            if (allLocations.length > 0) {
+                await getCharacterDetails(allLocations[0]);
+            }
         } catch (error) {
             setError(`No locations found. Please try a different search.`);
             console.error(`Error fetching location details in ${LocationPage}.tsx:`, error);
@@ -49,27 +61,36 @@ export const LocationPage = () => {
             setError("");
             setLoading(true);
 
-            const characterIds = selectedLocationDetails.residents
-            .map(url => url.split("/").pop())  // Extract character IDs from URLs
-            .filter(id => id !== undefined)   // Filter out undefined values
-            .map(id => parseInt(id!));        // Convert to numbers
+            if (selectedLocationDetails.residents.length === 0) {
+                setCharacterDetails([]); // or handle it as needed
+                setError(`No residents found for the selected location.`);
+                return;
+            }
 
-    
+            const characterIds = selectedLocationDetails.residents
+                .map(url => url.split("/").pop())  // Extract character IDs from URLs
+                .filter(id => id !== undefined)   // Filter out undefined values
+                .map(id => parseInt(id!));        // Convert to numbers
+
+
             const characterIdsString = characterIds.join(",");
             const apiUrl = `https://rickandmortyapi.com/api/character/${characterIdsString}`;
             const response = await fetch(apiUrl);
-    
+
             if (!response.ok) {
                 console.error(`Error fetching character details. Status: ${response.status}`);
                 setError(`Error fetching character details. Status: ${response.status}`);
                 return;
             }
-    
+
             const characterDetails2 = await response.json();
             console.log("Character Details:", characterDetails2);
-            setCharacterDetails(characterDetails2.filter(Boolean));
+
+            // Ensure characterDetails2 is always an array
+            const detailsArray = Array.isArray(characterDetails2) ? characterDetails2 : [characterDetails2];
+            setCharacterDetails(detailsArray);
         } catch (error) {
-            // setError(`Error fetching character details: ${error.message}`);
+            setError(`Error fetching character details: ${error}`);
             console.error("Error fetching character details:", error);
         } finally {
             setLoading(false);
@@ -79,17 +100,17 @@ export const LocationPage = () => {
     const handleSelectLocation = async (selectedLocation: string) => {
         setSelectedLocation(selectedLocation);
         console.log("Selected Location:", selectedLocation);
-    
+
         // Find the details of the selected location using the name
         const selectedLocationDetails = locations.find((location) => location.name === selectedLocation);
-    
+
         console.log("sid", selectedLocationDetails);
-        
+
         if (selectedLocationDetails) {
             await getCharacterDetails(selectedLocationDetails);
         }
     };
-    
+
     useEffect(() => {
         getLocationDetails();
     }, []);
@@ -100,7 +121,7 @@ export const LocationPage = () => {
         <div>
             <Selector
                 options={locations.map((location) => location.name)}
-                selectedOption={selectedLocation ? selectedLocation.name : ""}
+                selectedOption={selectedLocation ? selectedLocation : ""}
                 onSelectOption={handleSelectLocation}
             />
             {error ? (<div className="flex items-center justify-center p-8"> <p className="text-red-500 font-medium">{error}</p> </div>) : (
